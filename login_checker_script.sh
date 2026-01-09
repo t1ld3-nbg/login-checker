90bd59bb9568a895da05383d51693add4d615795
Tilde <norberg.tilde@gmail.com>
2026-01-09 12:46:27 +0100

    Corrected spelling and a comment.

--- login_checker_script.sh
+++ login_checker_script.sh
@@ -7,9 +7,9 @@ LOG_FILE="/var/log/login_checker.log"
 # Variable for IP-address.
 IP_ADDRESS=$(ip a show scope global | awk '/inet / {print $2}' | cut -d/ -f1 | head -n 1)
 
-# Logs time and date as well as which user logged in using what terminal.
+# Logs time and date as well as which user and their terminal and IP address.
 {
-	echo "$(date '+%Y-%m-%d %H:%M:%S')  User $USER logged in from $(tty) using IP adress $IP_ADDRESS"
+	echo "$(date '+%Y-%m-%d %H:%M:%S')  User $USER logged in from $(tty) using IP address $IP_ADDRESS"
 } >> "$LOG_FILE"
 exit 0
 
