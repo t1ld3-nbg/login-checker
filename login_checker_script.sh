9f314276bfea95b13be1574a78242944fdb7fae2
Tilde <norberg.tilde@gmail.com>
2026-01-09 01:11:16 +0100

    Corrected a spelling error, and some missing spaces.

--- login_checker_script.sh
+++ login_checker_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Login Checker logs all logins with date, time and user.
+# Login Checker logs all logins with date, time and user.
 
-#Saves the log output to a log file.
+# Saves the log output to a log file.
 LOG_FILE="/var/log/login_checker.log"
 
-# Logs time and date as well which user logged in using what terminal.
+# Logs time and date as well as which user logged in using what terminal.
 {
 	echo "$(date '+%Y-%m-%d %H:%M:%S')  User $USER logged in from $(tty)"
 } >> "$LOG_FILE"
